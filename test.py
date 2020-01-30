77859cea6141298f365b1bc1af5fc879b5e316ac
Teng Ma <asteedma@gmail.com>
2020-01-29 19:45:59 -0600

    New Test2

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 # %%
-print("你好")
-# %%
 msg = "hello"
 print(msg)
+
+
+def greeting(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greeting("world"))
